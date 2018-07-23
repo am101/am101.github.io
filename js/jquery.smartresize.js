3e88f60e4b2afe8949a7c1eab02262048c21a220
am101 <31003042+am101@users.noreply.github.com>
2018-07-23 03:13:36 -0700

    Add files via upload

---
+++ js/jquery.smartresize.js
@@ -0,0 +1,39 @@
+(function( window, $, undefined ) {
+	/*
+	* smartresize: debounced resize event for jQuery
+	*
+	* latest version and complete README available on Github:
+	* https://github.com/louisremi/jquery.smartresize.js
+	*
+	* Copyright 2011 @louis_remi
+	* Licensed under the MIT license.
+	*/
+
+	var $event = $.event, resizeTimeout;
+
+	$event.special.smartresize 	= {
+		setup: function() {
+			$(this).bind( "resize", $event.special.smartresize.handler );
+		},
+		teardown: function() {
+			$(this).unbind( "resize", $event.special.smartresize.handler );
+		},
+		handler: function( event, execAsap ) {
+			// Save the context
+			var context = this,
+				args 	= arguments;
+
+			// set correct event type
+			event.type = "smartresize";
+
+			if ( resizeTimeout ) { clearTimeout( resizeTimeout ); }
+			resizeTimeout = setTimeout(function() {
+				jQuery.event.handle.apply( context, args );
+			}, execAsap === "execAsap"? 0 : 50 );
+		}
+	};
+
+	$.fn.smartresize 			= function( fn ) {
+		return fn ? this.bind( "smartresize", fn ) : this.trigger( "smartresize", ["execAsap"] );
+	};
+})( window, jQuery );
